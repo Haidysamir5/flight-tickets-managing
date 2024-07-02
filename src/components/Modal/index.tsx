bc5d14d8851b8ea682acb9490e8c49fffdb07568
Haidysamir5 <40511123+Haidysamir5@users.noreply.github.com>
2024-07-02 22:54:32 +0300

    remove unused props from modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,13 +1,8 @@
 import {
   Backdrop,
   Grid,
-  useTheme,
-  createTheme,
   Fade,
-  CircularProgress,
   Typography,
-  Button,
-  makeStyles,
   Modal as MuiModal,
   Box,
   IconButton,
@@ -19,7 +14,6 @@ import { BoxStyle } from "./styles";
 export default function Modal({
   title,
   children,
-  isLoading,
   bodyOverFlow,
   open = false,
   setOpenModal,
@@ -43,58 +37,45 @@ export default function Modal({
         }}
       >
         <Fade in={open}>
-          {isLoading ? (
-            <Grid
-              direction="column"
-              className="modal-content loading-content"
-              container
-              justifyContent="center"
-              alignItems="center"
-              height="100vh"
-            >
-              <CircularProgress sx={{ color: "white" }} />
-            </Grid>
-          ) : (
-            <Box sx={BoxStyle}>
-              <Grid direction="column" className="modal-content" container>
-                <Grid
-                  direction="row"
-                  justifyContent="space-between"
-                  className="modal-header"
-                  wrap="nowrap"
-                  container
-                  alignItems="center"
-                  sx={{
-                    borderBottom: `1px solid secondary.main`,
-                  }}
-                >
-                  {title && <Typography>{title}</Typography>}
+          <Box sx={BoxStyle}>
+            <Grid direction="column" className="modal-content" container>
+              <Grid
+                direction="row"
+                justifyContent="space-between"
+                className="modal-header"
+                wrap="nowrap"
+                container
+                alignItems="center"
+                sx={{
+                  borderBottom: `1px solid secondary.main`,
+                }}
+              >
+                {title && <Typography>{title}</Typography>}
 
-                  <IconButton
-                    aria-label="close"
-                    size="medium"
-                    onClick={() => setOpenModal(false)}
-                  >
-                    <CloseIcon />
-                  </IconButton>
-                </Grid>
-                <Grid
-                  direction="column"
-                  justifyContent="space-between"
-                  className="modal-body"
-                  container
-                  sx={{
-                    padding: 2,
-                    borderBottom: `1px solid secondary`,
-                    maxHeight: "60vh",
-                    overflow: bodyOverFlow || "auto",
-                  }}
+                <IconButton
+                  aria-label="close"
+                  size="medium"
+                  onClick={() => setOpenModal(false)}
                 >
-                  {children}
-                </Grid>
+                  <CloseIcon />
+                </IconButton>
               </Grid>
-            </Box>
-          )}
+              <Grid
+                direction="column"
+                justifyContent="space-between"
+                className="modal-body"
+                container
+                sx={{
+                  padding: 2,
+                  borderBottom: `1px solid secondary`,
+                  maxHeight: "60vh",
+                  overflow: bodyOverFlow || "auto",
+                }}
+              >
+                {children}
+              </Grid>
+            </Grid>
+          </Box>
         </Fade>
       </MuiModal>
     </div>
--- src/components/Modal/types.ts
+++ src/components/Modal/types.ts
@@ -2,7 +2,6 @@ export type ModalProps = {
   title?: string;
   children: React.ReactNode;
   onModalSubmit?: () => void;
-  isLoading?: boolean;
   bodyOverFlow?: "visible" | "hidden " | "auto";
   open?: boolean;
   setOpenModal: (value: boolean) => void;
